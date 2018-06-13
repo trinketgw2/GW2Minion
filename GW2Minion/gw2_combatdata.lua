876011062aef6801eb7d0653b38b507807809038
fxfire <minionoffice@gmail.com>
2018-06-13 11:16:17 +0200

    puny fix

--- GW2Minion/gw2_combatdata.lua
+++ GW2Minion/gw2_combatdata.lua
@@ -583,7 +583,9 @@ function gw2_combatdata.cleanLog()
 			for targetid,targetLog in pairs(targets) do
 				if (targetLog.timeOfDeath > 0) then
 					if ((targetLog.timeOfDeath + 60000) < GetSystemTime()) then
-						gw2_combatdata.damageLog[sourceID][targetid] = nil
+						if (gw2_combatdata.damageLog[sourceID][targetid]) then
+							gw2_combatdata.damageLog[sourceID][targetid] = nil
+						end
 						gw2_combatdata.damageLog[targetid] = nil
 					end
 				elseif ((targetLog.timeOfLastHit + 30000) < GetSystemTime() and gw2_combatdata.damageLog[sourceID]) then
