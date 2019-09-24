a3bf984b1629837475ca0952cbdbaeba381aadd5
Jorith <jmzwaan@gmail.com>
2019-09-24 13:58:15 +0200

    - gw2_unstuck -- removed "canMove" check to trigger unstuck. Jumping/Falling is not being stuck, and we can move now

--- GW2Minion/gw2_unstuck.lua
+++ GW2Minion/gw2_unstuck.lua
@@ -116,10 +116,10 @@ function gw2_unstuck.HandleStuck()
 		return gw2_unstuck.lastresult
 	end
 	
-	if(not ml_global_information.Player_CanMove) then
-		gw2_unstuck.lastresult = true
-		return gw2_unstuck.lastresult
-	end
+	-- if(not ml_global_information.Player_CanMove) then
+		-- gw2_unstuck.lastresult = true
+		-- return gw2_unstuck.lastresult
+	-- end
 	
 	-- Make sure we are actually in the game and not a cutscene 
 	if(ml_global_information.GameState ~= GW2.GAMESTATE.GAMEPLAY) then
