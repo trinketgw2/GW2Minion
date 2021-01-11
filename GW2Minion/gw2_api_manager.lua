1f61eb3d72aa48f1d61fc2aef562d91f1ec42781
frenkey-derp <54779599+frenkey-derp@users.noreply.github.com>
2021-01-11 15:57:42 +0100

    Update gw2_api_manager.lua

--- GW2Minion/gw2_api_manager.lua
+++ GW2Minion/gw2_api_manager.lua
@@ -585,10 +585,10 @@ function api_manager.Load_API_Data()
    for _, foldername in pairs(FolderList(api_manager.path .. "API_Data", ".*", true)) do
       for _, file in pairs(FolderList(api_manager.path .. "API_Data\\" .. foldername, ".*.lua", false) or {}) do
          api_manager.API_Data[foldername] = api_manager.API_Data[foldername] or {}
-         api_manager.API_Data[foldername][string.trim(file, 4)] = FileLoad(api_manager.path .. foldername .. "\\" .. file) or {}
+         api_manager.API_Data[foldername][string.trim(file, 4)] = FileLoad(api_manager.path .. "API_Data\\" .. foldername .. "\\" .. file) or {}
 
          if not table.valid(api_manager.API_Data[foldername][string.trim(file, 4)]) then
-            FileDelete(api_manager.path .. foldername .. "\\" .. file)
+            FileDelete(api_manager.path .. "API_Data\\" .. foldername .. "\\" .. file)
             api_manager.API_Data[foldername][string.trim(file, 4)] = {}
          end
       end
